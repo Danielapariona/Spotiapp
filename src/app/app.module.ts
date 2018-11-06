a7e2d2f7951af46e1a0ec8945baf30ef5b7113e7
Danielapariona <daniela.parionac@gmail.com>
2018-11-06 17:51:29 -0500

    Create routes
    Signed-off-by: Danielapariona <daniela.parionac@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar rutas
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
