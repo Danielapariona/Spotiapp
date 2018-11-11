0cc5e4844cdb6bd2fb26acd4939f6648ce28d43a
Danielapariona <daniela.parionac@gmail.com>
2018-11-11 16:15:37 -0500

    Artist search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artists: any [] = [];
+  constructor(private searchService: SpotifyService) { }
 
-  ngOnInit() {
+  search(term: string) {
+    this.searchService.getArtist(term).subscribe((data: any) => {
+      this.artists = data.artists.items;
+    })
   }
-
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,4 +15,12 @@ export class SpotifyService {
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
   }
+
+  getArtist(term: string) {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBWNcbYWV8NdpaEbGXVcaN6DSb9cVHfU4X3gwSVsE7U6rWBNFtuEN8-7ltUGtBLYHD9w_uYDoFFP0cCj6E'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?query=${term}&type=artist&market=PE&offset=0&limit=15`, { headers });
+  }
 }
