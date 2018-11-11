bdc578ff6aa66301ba23620b7ff7a478e5ef9014
Danielapariona <daniela.parionac@gmail.com>
2018-11-11 16:49:04 -0500

    Centralize requests to Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,21 +9,21 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQBWNcbYWV8NdpaEbGXVcaN6DSb9cVHfU4X3gwSVsE7U6rWBNFtuEN8-7ltUGtBLYHD9w_uYDoFFP0cCj6E'
     });
+    return this.http.get(url, { headers });
+  }
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
                 .pipe( map( data => data['albums'].items));
   }
 
   getArtist(term: string) {
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBWNcbYWV8NdpaEbGXVcaN6DSb9cVHfU4X3gwSVsE7U6rWBNFtuEN8-7ltUGtBLYHD9w_uYDoFFP0cCj6E'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?query=${term}&type=artist&market=PE&offset=0&limit=15`, { headers })
-            .pipe( map( data =>  data['artists'].items));
+    return this.getQuery(`search?query=${term}&type=artist&market=PE&offset=0&limit=15`)
+                .pipe( map( data =>  data['artists'].items));
   }
 }
